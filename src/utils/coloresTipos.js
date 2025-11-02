ab899cb9446a94a7ff92cc35f4783501a3b697b9
Natalia18803 <natisss820@gmail.com>
2025-11-01 22:52:53 -0500

    .

--- src/utils/coloresTipos.js
+++
@@ -1,9 +0,0 @@
-// src/utils/coloresTipos.js
-export default {
-  normal: "#A8A77A",
-  fire: "#EE8130",
-  water: "#6390F0",
-  grass: "#7AC74C",
-  electric: "#F7D02C",
-  // add other types/colors as needed
-};
